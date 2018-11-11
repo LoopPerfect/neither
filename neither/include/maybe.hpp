3b6ca6ee267b0bb329ca43a22444793386ed8bf4
rvarago <varago.rafael@gmail.com>
2018-11-11 16:56:07 +0100

    Maybe: Add static const object none
    That is an instance of type Maybe<void>, used to represent the absence
    of a value.
    
    Additionally, added support for operator== and operator!= for
    Maybe<void>, hence none, with the semantics:
    
    Maybe<void> x;
    Maybe<void> y;
    
    x == y => true
    x != y => false
    
    Given that it doesn't hold a value.
    
    Therefore, none will be always equal to itself.

--- neither/include/maybe.hpp
+++ neither/include/maybe.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cassert>
 #include <cstddef>
+#include <type_traits>
 #include <neither/traits.hpp>
 
 namespace neither {
@@ -106,18 +107,33 @@ auto maybe(T value) -> Maybe<T> { return {value}; }
 template <typename T = void>
 auto maybe() -> Maybe<T> { return {}; }
 
+namespace {
+
+    template<typename T, typename std::enable_if_t<!std::is_void<T>::value>* = nullptr>
+    bool equal(Maybe<T> const &a, Maybe<T> const &b) {
+        if (a.hasValue) {
+            return b.hasValue && a.value == b.value;
+        }
+        return !b.hasValue;
+    }
+
+    template <typename T, typename std::enable_if_t<std::is_void<T>::value>* = nullptr>
+    bool equal(Maybe<T> const&, Maybe<T> const&) {
+        return true;
+    }
+}
+
 template <typename T>
 bool operator == (Maybe<T> const& a, Maybe<T> const& b) {
-  if (a.hasValue) {
-    return b.hasValue && a.value == b.value;
-  }
-  return !b.hasValue;
+  return equal(a, b);
 }
 
 template <typename T>
 bool operator != (Maybe<T> const& a, Maybe<T> const& b) {
   return !(a == b);
 }
+
+static const auto none = maybe();
 
 }
 
