95c855c60c3bdab04b9d909d5b358581a8eef2d3
njlr <njlr@users.noreply.github.com>
2019-01-09 17:00:47 +0000

    * Upgrades to newer Buck

--- travis/before-install-linux.sh
+++ travis/before-install-linux.sh
@@ -9,7 +9,7 @@ gcc --version
 
 sudo apt-get install -y equivs openjdk-8-jdk
 
-wget -O buck.deb https://github.com/facebook/buck/releases/download/v2018.08.27.01/buck.2018.08.27.01_all.deb
+wget -O buck.deb https://github.com/facebook/buck/releases/download/v2018.10.29.01/buck.2018.10.29.01_all.deb
 sudo dpkg -i buck.deb
 buck --version
 
